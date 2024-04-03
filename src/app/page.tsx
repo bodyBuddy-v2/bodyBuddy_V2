de3f70c4edfb9ceb3b720433a723efa3cbd977db
Park sian <52738906+sians5544@users.noreply.github.com>
2024-04-03 21:56:41 +0900

    fix : typography 컴포넌트 옵션 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,24 @@ import Typography from "@/components/Typography";
 export default async function Home() {
   return (
     <div>
-      <Typography variant="h2">바디버디홈이양</Typography>
-
-      <Button variant="outlined" color="primary" sx={{ color: "primary.main" }}>
-        Home
+      <Typography variant="h1">h1</Typography>
+      <Typography variant="h2">h2</Typography>
+      <Typography variant="h3">h3</Typography>
+      <Typography variant="h4">h4</Typography>
+      <Typography variant="subtitle1">subtitle1 - ex. 게시판 제목</Typography>
+      <Typography variant="subtitle2">
+        subtitle2 - ex. 상세정보, 수상경력 , 위치
+      </Typography>
+      <Typography variant="body1">body1 & div</Typography>
+      <div>
+        <Typography variant="body2">body2 & span</Typography>
+      </div>
+      <Typography variant="caption">caption - ex. 자격 및 수상 정보</Typography>
+      <Button variant="outlined" color="primary">
+        primary
+      </Button>
+      <Button variant="outlined" color="info">
+        info
       </Button>
     </div>
   );
--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -1,10 +1,8 @@
+"use client";
 import { Typography as MuiTypography, styled } from "@mui/material";
 import type { TypographyProps } from "@mui/material";
-///디자인 프롭을 어케 맞춰야할까...???
 
-export type TypographyType = {
-  /** custom으로 들어올 수 있는 아이들 */
-} & TypographyProps;
+export type TypographyType = {} & TypographyProps;
 
 const Typography = (props: TypographyProps) => {
   const { children, ...others } = props;
@@ -16,6 +14,12 @@ export default Typography;
 
 const StyledTypography = styled(MuiTypography)(({ variant }) => {
   return {
-    ...(variant === "h2" && { fontWeight: 700, fontSize: "44px" }),
+    ...(variant === "h1" && { fontWeight: 700, fontSize: "18px" }),
+    ...(variant === "h2" && { fontWeight: 400, fontSize: "12px" }),
+    ...(variant === "h3" && { fontWeight: 700, fontSize: "30px" }),
+    ...(variant === "subtitle1" && { fontWeight: 800, fontSize: "16px" }),
+    ...(variant === "subtitle2" && { fontWeight: 800, fontSize: "14px" }),
+    ...(variant === "button" && { fontWeight: 700, fontSize: "20px" }),
+    ...(variant === "caption" && { fontWeight: 400, fontSize: "12px" }),
   };
 });
