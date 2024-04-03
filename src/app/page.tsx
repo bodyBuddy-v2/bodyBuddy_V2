cf519274364c4d06fea9db5c3efb96cc977dbefe
PARK MINJI <65998556+mei-zy@users.noreply.github.com>
2024-04-03 21:47:28 +0900

    [chore] github workflow 설정 (#17)
    * chore: workflow (타입체크) 테스트
    
    * docs: 엔터 추가
    
    * fix: eslint 체크

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@mui/material";
 
-export default async function Home() {
+const Home = () => {
   return (
     <div>
       바디버디홈이양
       <Button variant="outlined">Outlined</Button>
     </div>
   );
-}
+};
+
+export default Home;
