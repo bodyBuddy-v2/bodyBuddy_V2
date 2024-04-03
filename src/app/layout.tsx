9c1a1d7b765da17a0aa07d48ea69e5d0ee3da609
Park sian <52738906+sians5544@users.noreply.github.com>
2024-04-03 21:56:06 +0900

    fix : theme 전체 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,10 @@
 import Providers from "@/components/Providers";
 import { Container } from "@mui/material";
 import StyledLayout from "@/components/StyledLayout";
-import { global } from "@/styles/globals";
-import { Global } from "@emotion/react";
 import SEO from "@/components/SEO";
 import { ThemeProvider } from "@mui/material/styles";
 import Theme from "@/components/Theme";
+import CssBaseline from "@mui/material/CssBaseline";
 
 export default function RootLayout({
   children,
@@ -18,9 +17,9 @@ export default function RootLayout({
       <html lang="en">
         <SEO />
         <body>
-          <Global styles={global} />
           <Providers>
             <ThemeProvider theme={Theme}>
+              <CssBaseline />
               <Container maxWidth="sm">
                 <StyledLayout>{children}</StyledLayout>
               </Container>
--- src/components/Theme.tsx
+++ src/components/Theme.tsx
@@ -1,6 +1,71 @@
 import { createTheme } from "@mui/material";
 
 export const Theme = createTheme({
+  typography: {
+    fontFamily: "NanumSquareRound",
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        "@font-face": {
+          fontFamily: "NanumSquareRound",
+          src: 'url(https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_two@1.0/NanumSquareRound.woff) format("woff")',
+          fontWeight: "normal",
+          fontStyle: "normal",
+        },
+        html: {
+          maxWidth: "100%",
+          overflowX: "hidden",
+        },
+        body: {
+          maxWidth: "100%",
+          overflowX: "hidden",
+        },
+        input: {
+          "&:focus": {
+            outline: "none",
+          },
+        },
+        select: {
+          "&:focus": {
+            outline: "none",
+          },
+        },
+        option: {
+          "&:focus": {
+            outline: "none",
+          },
+        },
+        textarea: {
+          "&:focus": {
+            outline: "none",
+          },
+        },
+        button: {
+          "&:focus": {
+            outline: "none",
+          },
+        },
+        ".srOnly": {
+          overflow: "hidden",
+          position: "absolute",
+          clip: "rect(0, 0, 0, 0)",
+          clipPath: "inset(50%)",
+          width: "1px",
+          height: "1px",
+          margin: "-1px",
+        },
+      },
+    },
+    MuiTypography: {
+      defaultProps: {
+        variantMapping: {
+          body1: "div",
+          body2: "span",
+        },
+      },
+    },
+  },
   palette: {
     text: {
       primary: "#464646",
