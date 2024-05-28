afa1dba079fd7501e3b8b978c905f8e2582ac078
Park sian <52738906+sians5544@users.noreply.github.com>
2024-05-28 23:30:17 +0900

    docs : storybook path 세팅 수정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import path from "path";
 
 const config: StorybookConfig = {
   stories: ["../src/components/**/*.mdx", "../src/components/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -12,6 +13,15 @@ const config: StorybookConfig = {
     name: "@storybook/nextjs",
 
     options: {},
+  },
+  webpackFinal: async config => {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "@": path.resolve(__dirname, "../src"), // 프로젝트의 src 폴더 경로를 지정합니다.
+      };
+    }
+    return config;
   },
   docs: {
     autodocs: "tag",
