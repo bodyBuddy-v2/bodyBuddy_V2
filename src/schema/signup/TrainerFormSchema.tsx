ecb4892d7ccc8f993a6b865160bf5abdc7638d28
Park sian <52738906+sians5544@users.noreply.github.com>
2024-07-29 22:19:02 +0900

    fix :분리한 스키마 통합으로 수정

--- src/schema/signup/TrainerFormSchema.tsx
+++ src/schema/signup/TrainerFormSchema.tsx
@@ -1,5 +1,5 @@
 import { TrainerFormKey } from "@/constant/common/formKey";
-import { array, object, string } from "yup";
+import { object, string } from "yup";
 import { exerciseList, fieldList } from "@/constant/common/signup";
 
 const TrainerFormSchema = () => {
@@ -8,49 +8,7 @@ const TrainerFormSchema = () => {
       .oneOf(exerciseList, "유효한 종목을 선택해주세요.")
       .required("종목을 선택해 주세요."),
     [TrainerFormKey.FIELD]: string().oneOf(fieldList, "유효한 분야를 선택해주세요.").required("분야를 선택해 주세요."),
-    // [TrainerFormKey.RROFILEIMG]: array()
-    //   .of(
-    //     object().shape({
-    //       File: mixed().required(),
-    //     }),
-    //   )
-    //   .required(),
     [TrainerFormKey.COMMENT]: string().required("프로필 코멘트를 입력해주세요.").max(20, "최대 20자를 초과했습니다!"),
-    [TrainerFormKey.TRAININGNAME]: string().required(""),
-    [TrainerFormKey.TRAININGIMG]: array()
-      .of(
-        object().shape({
-          name: string().required(),
-          url: string().required(),
-        }),
-      )
-      .required(),
-    [TrainerFormKey.TRAININGPATH]: string(),
-    [TrainerFormKey.COST]: string()
-      .required("비용을 입력해주세요.")
-      .matches(/^[0-9,]+$/, "숫자만 입력 가능합니다."),
-    [TrainerFormKey.YEAR]: string().required("년도를 입력해주세요."),
-    [TrainerFormKey.MONTH]: string().required("월을 입력해주세요."),
-    [TrainerFormKey.CERTIFICATIONS]: array().of(
-      object().shape({
-        name: string().required(),
-        url: string().required(),
-      }),
-    ),
-  });
-};
-const TrainerSchemaStep1 = () => {
-  return object().shape({
-    [TrainerFormKey.CATEGORY]: string()
-      .oneOf(exerciseList, "유효한 종목을 선택해주세요.")
-      .required("종목을 선택해 주세요."),
-    [TrainerFormKey.FIELD]: string().oneOf(fieldList, "유효한 분야를 선택해주세요.").required("분야를 선택해 주세요."),
-    [TrainerFormKey.COMMENT]: string().required("프로필 코멘트를 입력해주세요.").max(20, "최대 20자를 초과했습니다!"),
-  });
-};
-
-const TrainerSchemaStep3 = () => {
-  return object().shape({
     [TrainerFormKey.COST]: string()
       .required("비용을 입력해주세요.")
       .matches(/^[0-9,]+$/, "숫자만 입력 가능합니다."),
@@ -59,4 +17,4 @@ const TrainerSchemaStep3 = () => {
   });
 };
 
-export { TrainerSchemaStep1, TrainerSchemaStep3 };
+export { TrainerFormSchema };
