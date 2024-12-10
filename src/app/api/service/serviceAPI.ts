ecad3dcbffe6ce5afffd8beefb1b31af328748d1
mei-zy <alswlkku@gmail.com>
2024-12-10 21:06:21 +0900

    feat: 상세 옵션 설정 type 지정

--- src/app/api/service/serviceAPI.ts
+++ src/app/api/service/serviceAPI.ts
@@ -1,5 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
+import { GetUserDetailOptionRequest } from "./types";
+
 const serviceAPI = () => {
   const supabase = createClient("", "");
 
@@ -7,6 +9,8 @@ const serviceAPI = () => {
     getTrainerList: async () => {
       return await supabase.from("trainer").select("*");
     },
+    // user가 설정한 상세 옵션 설정
+    getUserDetailOption: async ({ id }: GetUserDetailOptionRequest) => {},
   };
 };
 
--- src/app/api/service/types.ts
+++ src/app/api/service/types.ts
@@ -1,9 +1,16 @@
+type Category = "physicalStrength" | "diet" | "muscle" | "bodyAlignment";
+type Gender = "male" | "female";
+
 export type Trainer = {
   name: string;
 };
 
+export type GetUserDetailOptionRequest = {
+  id: string;
+};
+
 export type GetTrainerListRequest = {
-  category: "physicalStrength" | "diet" | "muscle" | "bodyAlignment";
+  category?: Category;
 };
 
 export type GetTrainerListResponse = Trainer[];
