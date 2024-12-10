91164c5ce4d5d64cdf72ffc4a13fb1ecd6b64c56
mei-zy <alswlkku@gmail.com>
2024-12-10 21:40:32 +0900

    feat: type 추가

--- src/app/api/service/types.ts
+++ src/app/api/service/types.ts
@@ -1,5 +1,8 @@
 type Category = "physicalStrength" | "diet" | "muscle" | "bodyAlignment";
+type Field = "pt" | "yogaAndPilates" | "swimming" | "dance" | "golf" | "tennis";
 type Gender = "male" | "female";
+// user 선호 성별
+type PreferGender = Gender | "none";
 
 export type Trainer = {
   name: string;
@@ -14,3 +17,11 @@ export type GetTrainerListRequest = {
 };
 
 export type GetTrainerListResponse = Trainer[];
+
+export type GetUserDetailOptionResponse = {
+  city: string;
+  district: string;
+  prefer_gender: PreferGender;
+  category: Category[];
+  field: Field[];
+};
